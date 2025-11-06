e817866716badd4694eed8fa36268b1c3298c5c6
Djangodv <dd.manders@protonmail.com>
2025-11-06 08:58:00 +0100

    Fixed filescache and added **/managed_components/**

--- nvim/lua/config/fzf.lua
+++ nvim/lua/config/fzf.lua
@@ -2,16 +2,18 @@
 
 -- .git/* matches only the directories and files inside .git/ not e.g. .git/src/file.py (non-recursive)
 -- .git/** matches both
-vim.opt.wildignore = { ".git/**", "**/build/**", "**/.cache/**", "**/venv/**" }
+vim.opt.wildignore = { ".git/**", "**/build/**", "**/.cache/**", "**/venv/**", "**/managed_components/**" }
 -- NOTE: Directories will be searched with '/*' after them, due to limitations of globpath()
 vim.opt.path = { "/home/user/Github/", "/home/user/Github/dotfiles/", "/home/user/Github/dotfiles/nvim/", "/home/user/Github/dotfiles/nvim/lua/config" }
+
+local filescache = {}
 
 function Find(args, cmdcomplete)
 
   if vim.tbl_isempty(filescache) then
 
     -- Wildignore is only applied when {nosuf} is false (very confusing docs)
-    -- local cwd = vim.fn.getcwd()
+    local cwd = vim.fn.getcwd()
     filescache = vim.fn.globpath('.', '**', false, true)
 
     -- Globpath expects a list of strings so the table returned by path has to be concentated
@@ -27,9 +29,9 @@ function Find(args, cmdcomplete)
 
     -- When you call a vim function from Lua, neovim automatically converts vimscript lists to Lua tables
     -- Set path relative to cwd (redundant in combination w. au for setting cwd)
-    local filescache = vim.tbl_map(function(path) 
-      return vim.fn.fnamemodify(path, ':.')
-    end, filescache)
+    -- local filescache = vim.tbl_map(function(path) 
+    --   return vim.fn.fnamemodify(path, ':.')
+    -- end, filescache)
   end
   return args == '' and filescache or vim.fn.matchfuzzy(filescache, args)
 end
@@ -37,7 +39,7 @@ end
 vim.opt.findfunc = 'v:lua.Find'
 
 -- Empty cache file each time the workspace au sets the directory
-vim.api.nvim_create_autocmd('DirChanged', {
+vim.api.nvim_create_autocmd({'DirChanged', 'VimEnter'}, {
     pattern = "window",
     callback = function() 
       filescache = {} 
